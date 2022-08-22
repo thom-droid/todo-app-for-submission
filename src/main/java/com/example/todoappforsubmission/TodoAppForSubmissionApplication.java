44f96ef18b04d0d4ea5acadd749a0db9ba288e5c
thom-window <hsbang.thom@gmail.com>
2022-08-22 11:17:09 +0900

    intiial

---
+++ src/main/java/com/example/todoappforsubmission/TodoAppForSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappforsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppForSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppForSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoappforsubmission/TodoAppForSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappforsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppForSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
