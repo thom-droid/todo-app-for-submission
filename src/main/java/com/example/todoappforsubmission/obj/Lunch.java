27d432d3fc85b6c2b70c73c04b8779ff3504a8b9
thom-window <hsbang.thom@gmail.com>
2022-08-22 12:00:01 +0900

    feat : add a bean to print out my lunch
    - Add Lunch that is important for my life.

---
+++ src/main/java/com/example/todoappforsubmission/obj/Lunch.java
@@ -0,0 +1,18 @@
+package com.example.todoappforsubmission.obj;
+
+public class Lunch {
+
+    private String name;
+
+    public Lunch(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
